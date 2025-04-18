4862c8948c4f0fe8ddf0ddb09e1d21bc738f6824
Guillaume de Rouville <guillaume@dagger.io>
2025-04-18 04:20:31 +0000

    npm: module prompt engineering
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

--- toolkit/npm/main.go
+++ toolkit/npm/main.go
@@ -1,16 +1,4 @@
-// A generated module for Npm functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
+// NPM related tools that works with any given source directory
 
 package main
 
@@ -20,26 +8,26 @@ import (
 	"npm/internal/dagger"
 )
 
-type Npm struct{
-	Ctr *dagger.Container
+type Npm struct {
+	Ctr    *dagger.Container
+	Source *dagger.Directory
 }
 
+// NPM related tools that works with any given source directory
 func New(
 	// +optional
 	ctr *dagger.Container,
+	// +defaultPath="/"
+	source *dagger.Directory,
 ) *Npm {
 	if ctr == nil {
 		ctr = dag.Container().From("node")
 	}
-	return &Npm{ctr}
+	return &Npm{ctr, source}
 }
 
 // Coverage runs the Vitest coverage command and returns its stdout
-func (m *Npm) Coverage(
-	ctx context.Context,
-	// +defaultPath="/"
-	source *dagger.Directory,
-) (string, error) {
+func (m *Npm) Coverage(ctx context.Context) (string, error) {
 	// TODO: add npm cache ?
-	return m.Ctr.WithMountedDirectory("/src", source).WithWorkdir("/src").WithExec([]string{"npx", "vitest", "run", "--coverage"}).Stdout(ctx)
+	return m.Ctr.WithMountedDirectory("/src", m.Source).WithWorkdir("/src").WithExec([]string{"npx", "vitest", "run", "--coverage"}).Stdout(ctx)
 }
