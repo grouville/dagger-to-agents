0e22292cb583034312a41fc061c8426bfa2d0521
Guillaume de Rouville <guillaume@dagger.io>
2025-05-02 20:01:16 +0000

    chore: update mcp.sh with latest version from tibor that handles signals
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

--- hello-dagger/.dagger/mcp.sh
+++ hello-dagger/.dagger/mcp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-tee /tmp/debug.stdin.log | OPENAI_API_KEY=toto dagger -m /target mcp --env-privileged 2>/tmp/debug.stderr.log | /usr/bin/tee /tmp/debug.stdout.log
+tee /tmp/debug.stdin.log | OPENAI_API_KEY=toto dagger -m /target mcp --env-privileged 2>/tmp/debug.stderr.log | /usr/bin/tee /tmp/debug.stdout.log &
+wait
