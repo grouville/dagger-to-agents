847f2c7d66709af444f8f9b671591c8b59bd4de1
Tibor Vass <teabee89@gmail.com>
2025-05-08 15:58:34 -0500

    Remove installDependencies
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- toolkit/npm/main.go
+++ toolkit/npm/main.go
@@ -23,21 +23,12 @@ func New(
 	return &Npm{ctr}
 }
 
-func (m *Npm) installDependencies(ctx context.Context, source *dagger.Directory) (*dagger.Container, error) {
-	// Install vitest and coverage dependencies
-	return m.Ctr.
+// Coverage runs the Vitest coverage command on the provided source directory and returns its stdout
+func (m *Npm) Coverage(ctx context.Context, source *dagger.Directory) (string, error) {
+	ctr := m.Ctr.
 		WithMountedDirectory("/src", source).
 		WithWorkdir("/src").
 		WithExec([]string{"npm", "install", "--save-dev", "vitest", "@vitest/coverage-v8"})
-}
-
-// Coverage runs the Vitest coverage command on the provided source directory and returns its stdout
-func (m *Npm) Coverage(ctx context.Context, source *dagger.Directory) (string, error) {
-	// Ensure dependencies are installed
-	ctr, err := m.installDependencies(ctx, source)
-	if err != nil {
-		return "", err
-	}
 
 	// Run the coverage command
 	return ctr.
